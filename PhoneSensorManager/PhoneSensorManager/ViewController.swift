d73fc2541ac4c1619af94cf50c0e5110534f2919
Larry Li <larrylisky@gmail.com>
2019-12-11 10:28:18 -0600

    First check-in

---
+++ PhoneSensorManager/PhoneSensorManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhoneSensorManager
+//
+//  Created by Larry Li on 12/11/19.
+//  Copyright © 2019 e-motion.ai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
