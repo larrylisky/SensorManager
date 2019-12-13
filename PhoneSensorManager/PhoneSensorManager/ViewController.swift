6b7092ecc3f92d034123984cc4e4d9da52ff2241
Larry Li <larrylisky@gmail.com>
2019-12-13 07:28:42 -0600

    Added graph picker support

--- PhoneSensorManager/PhoneSensorManager/ViewController.swift
+++ PhoneSensorManager/PhoneSensorManager/ViewController.swift
@@ -22,29 +22,64 @@ class ViewController: UIViewController {
     @IBOutlet weak var exportFileName: UITextField!
     @IBOutlet weak var recordButton: UIButton!
     
+    // Reference frames display params
     var scene = SCNScene()
     var cameraNode = SCNNode()
     var geometry : SCNGeometry!
-    
     var phoneRefFrameNode : SCNNode!
     var earthRefFrameNode : SCNNode!
     var carRefFrameNode : SCNNode!
-
     var phoneRefFrame : Mesh!
     var earthRefFrame : Mesh!
     var carRefFrame : Mesh!
-
-    var defaultColor : UIColor!
     let sensor = SensorManager()
-    var angle : Float = 0.0
+
+    // UI params
+    var hideRefFrameView = false
+    var defaultColor : UIColor!
+    var graphIndex : Int = 0
+    var pickerData = [
+        "Reference frames",
+        "Acceleration",
+        "Speed"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         recordButton.setTitle("Record", for: .normal)
         defaultColor = recordButton.titleColor(for: .normal)
         
+        // Set up graph picker
+        graphPickView.delegate = self
+        graphPickView.dataSource = self
+        
+        // Setup reference frame display
+        if !hideRefFrameView {
+            setupRefFrameView()
+        }
+        
+        // Setup periodic timer task
+        _ = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(periodic), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func onClearPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onExportPressed(_ sender: Any) {
+    }
+    
+    @IBAction func onRecordToggled(_ sender: Any) {
+        if recordButton.titleLabel?.text == "Record" {
+            recordButton.setTitleColor(UIColor.red, for: .normal)
+            recordButton.setTitle("Stop", for: .normal)
+        }
+        else {
+            recordButton.setTitleColor(defaultColor, for: .normal)
+            recordButton.setTitle("Record", for: .normal)
+        }
+    }
+    
+    func setupRefFrameView() {
         // Setup scene and camera
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
@@ -88,81 +123,48 @@ class ViewController: UIViewController {
         sceneView.allowsCameraControl = false
         sceneView.showsStatistics = false
         sceneView.backgroundColor = .white
-        
-        // Setup periodic timer task
-        _ = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(pollAttitude), userInfo: nil, repeats: true)
     }
     
-    @IBAction func onClearPressed(_ sender: Any) {
-    }
     
-    @IBAction func onExportPressed(_ sender: Any) {
-    }
-    
-    @IBAction func onRecordToggled(_ sender: Any) {
-        if recordButton.titleLabel?.text == "Record" {
-            recordButton.setTitleColor(UIColor.red, for: .normal)
-            recordButton.setTitle("Stop", for: .normal)
-        }
-        else {
-            recordButton.setTitleColor(defaultColor, for: .normal)
-            recordButton.setTitle("Record", for: .normal)
+    func drawRefFrame(r: CMRotationMatrix) {
+        if graphIndex == 0 || graphIndex == 2 {
+            let arg = 1 + r.m11 + r.m22 + r.m33
+            if arg > 0.0000001 {
+                let qw = sqrt(arg)/2
+                let qx = (r.m32 - r.m23) / (4*qw)
+                let qy = (r.m13 - r.m31) / (4*qw)
+                let qz = (r.m21 - r.m12) / (4*qw)
+                earthRefFrameNode.orientation = SCNQuaternion(qx, qy, qz, qw)
+            }
         }
     }
     
-    func drawRefFrame(earth: CMRotationMatrix, car: CMRotationMatrix) {
-        SCNTransaction.begin()
-        
-        var r = earth
-        var arg = 1 + r.m11 + r.m22 + r.m33
-        if arg > 0.0000001 {
-            let qw = sqrt(arg)/2
-            let qx = (r.m32 - r.m23) / (4*qw)
-            let qy = (r.m13 - r.m31) / (4*qw)
-            let qz = (r.m21 - r.m12) / (4*qw)
-            earthRefFrameNode.orientation = SCNQuaternion(qx, qy, qz, qw)
+    
+    @objc func periodic() {
+        if !sceneView.isHidden {
+            updateScene()
         }
-        
-        r = car
-        arg = 1 + r.m11 + r.m22 + r.m33
-        if arg > 0.0000001 {
-            let qw = sqrt(arg)/2
-            let qx = (r.m32 - r.m23) / (4*qw)
-            let qy = (r.m13 - r.m31) / (4*qw)
-            let qz = (r.m21 - r.m12) / (4*qw)
-            carRefFrameNode.orientation = SCNQuaternion(qx, qy, qz, qw)
-        }
-        
-        SCNTransaction.commit()
     }
-    
-    
-    @objc func pollAttitude() {
-        
-        // Ground reference frame has Z axis pointing up, X axis pointing North.
-        // According to, East has a course of 90 deg, that is counter-clockwise (negative)
-        // rotation about the Z axis - righ hand rule
-        // negative course value is invalid
 
-      //  let course = 280.0
-      //  let speed = 1.0
+    
+    func updateScene() {
         let course = sensor.data.course
-        let speed = sensor.data.speed
+        var RotationCP : CMRotationMatrix
+        let RotationGP = sensor.rotationMatrix
         
-        if course >= 0.0  {
-            let theta = -course / 180.0 * Double.pi  //Course in radian from -pi to +pi
+        SCNTransaction.begin()
+        drawRefFrame(r: RotationGP)
+        if course >= 0  {
+            let theta = -course / 180.0 * Double.pi
             let c = cos(theta)
             let s = sin(theta)
-            print("course = \(course), theta = \(theta) speed = \(speed)")
             let RotationGC = CMRotationMatrix(m11: c, m12:-s, m13: 0,
                                               m21: s, m22: c, m23: 0,
                                               m31: 0, m32: 0, m33: 1)
-        
-            // Rgp = Rgc * Rcp; therefore, Rcp = inv(Rgc) * Rgp
-            let RotationGP = sensor.rotationMatrix
-            let RotationCP = RotationGP * RotationGC
-            drawRefFrame(earth: RotationGP, car: RotationCP)
+            RotationCP = RotationGP * RotationGC
+            drawRefFrame(r: RotationCP)
         }
+        SCNTransaction.commit()
     }
     
     func arrow(length: Double, color: UIColor) -> Mesh {
@@ -172,10 +174,35 @@ class ViewController: UIViewController {
         return mesh
     }
     
+    
     func refFrame(size: Double, alpha: Double)-> Mesh {
         let axisX = arrow(length: size, color: UIColor(red: 1, green: 0, blue: 0, alpha: CGFloat(alpha))).rotZ(deg: 90)
         let axisY = arrow(length: size, color: UIColor(red: 0, green: 1, blue: 0, alpha: CGFloat(alpha)))
         let axisZ = arrow(length: size, color: UIColor(red: 0, green: 0, blue: 1, alpha: CGFloat(alpha))).rotX(deg: -90)
         return axisX.merge(axisY.merge(axisZ))
+    }
+}
+
+// UIPickerViewDelegate, UIPickerViewDataSource extension
+extension ViewController : UIPickerViewDelegate, UIPickerViewDataSource {
+    // Number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    // Capture the picker view selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        graphIndex = row
+        sceneView.isHidden = (graphIndex != 0)
     }
 }
